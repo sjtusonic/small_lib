8194d5531aef2b2a193796aa44df9745ae6d6adc
qq_linux <qq@ubuntu.(none)>
2015-10-03 00:44:45 -0700

    finish ver1

--- include.tcl
+++ include.tcl
@@ -356,6 +356,90 @@ proc vec_length_manhattan {v1} {
 
 ###6.1 VECTOR/BBOX TRANSFORM
 proc v2b {vec4} { ;# convert vec={1 4 3 2} to regular bbox {{1 2} {3 4}} (regular bbox is {{llx lly} {urx ury}})
+   set a [lindex [vec2points $vec4] 0]
+   set c [lindex [vec2points $vec4] 2]
+   return [list $a $c]
+}
+proc b2v {bbox} {;# this bbox may be not regular
+   #set tmp0_0 [lindex $bbox 0 0];#llx
+   #set tmp0_1 [lindex $bbox 0 1];#lly
+   #set tmp1_0 [lindex $bbox 1 0];#urx
+   #set tmp1_1 [lindex $bbox 1 1];#ury
+
+   #set llx [stat_min [list $tmp0_0 $tmp1_0]]
+   #set urx [stat_max [list $tmp0_0 $tmp1_0]]
+   #set lly [stat_min [list $tmp0_1 $tmp1_1]]
+   #set ury [stat_max [list $tmp0_1 $tmp1_1]]
+   set tmp [bbox2points $bbox]
+   return [concat [lindex $tmp 0] [lindex $tmp 2]]
+}
+
+###6.2 BBOX
+proc calc_area {bbox} {
+   set points [bbox2points $bbox]
+   set a [lindex $points 0]
+   set c [lindex $points 2]
+
+   set llx [lindex $a 0 ];#llx
+   set lly [lindex $a 1 ];#lly
+   set urx [lindex $c 0 ];#urx
+   set ury [lindex $c 1 ];#ury
+   return [expr ($urx-$llx)*($ury-$lly)]
+}
+
+proc point_is_in_bbox {p bbox {generalized 0}} {
+   set points [bbox2points $bbox]
+   set a [lindex $points 0]
+   set c [lindex $points 2]
+   set llx [lindex $a 0 ];#llx
+   set lly [lindex $a 1 ];#lly
+   set urx [lindex $c 0 ];#urx
+   set ury [lindex $c 1 ];#ury
+
+   set px [lindex $p 0]
+   set py [lindex $p 1]
+
+   if {$generalized} {
+      if {  $px>=$llx 
+         && $px<=$urx
+         && $py>=$lly
+         && $py<=$ury
+         } {
+         return true;
+      }
+   } else {
+      if {  $px> $llx 
+         && $px< $urx
+         && $py> $lly
+         && $py< $ury
+         } {
+         return true;
+      }
+   }
+   return false
+}
+
+proc bbox2points {bbox} {
+   set tmp0_0 [lindex $bbox 0 0];#llx
+   set tmp0_1 [lindex $bbox 0 1];#lly
+   set tmp1_0 [lindex $bbox 1 0];#urx
+   set tmp1_1 [lindex $bbox 1 1];#ury
+
+   set llx [stat_min [list $tmp0_0 $tmp1_0]]
+   set urx [stat_max [list $tmp0_0 $tmp1_0]]
+   set lly [stat_min [list $tmp0_1 $tmp1_1]]
+   set ury [stat_max [list $tmp0_1 $tmp1_1]]
+   
+   set a [list $llx $lly]
+   set b [list $urx $lly]
+   set c [list $urx $ury]
+   set d [list $llx $ury]
+#  d---c
+#  |   |
+#  a---b
+   return [list $a $b $c $d]
+}
+proc vec2points {vec4} {
    set v0 [lindex $vec4 0]
    set v1 [lindex $vec4 1]
    set v2 [lindex $vec4 2]
@@ -367,20 +451,323 @@ proc v2b {vec4} { ;# convert vec={1 4 3 2} to regular bbox {{1 2} {3 4}} (regula
    set y_max [stat_max [list $v1 $v3]]
 
    set a [list $x_min $y_min]
-   set b [list $x_max $y_max]
-   return [list $a $b]
-}
-proc b2v {bbox} {;# this bbox may be not regular
-   set tmp0_0 [lindex $bbox 0 0];#llx
-   set tmp0_1 [lindex $bbox 0 1];#lly
-   set tmp1_0 [lindex $bbox 1 0];#urx
-   set tmp1_1 [lindex $bbox 1 1];#ury
-
-   set llx [stat_min [list $tmp0_0 $tmp1_0]]
-   set urx [stat_max [list $tmp0_0 $tmp1_0]]
-   set lly [stat_min [list $tmp0_1 $tmp1_1]]
-   set ury [stat_max [list $tmp0_1 $tmp1_1]]
-   return [list $llx $lly $urx $ury]
+   set b [list $x_max $y_min]
+   set c [list $x_max $y_max]
+   set d [list $x_min $y_max]
+#  d---c
+#  |   |
+#  a---b
+   return [list $a $b $c $d]
 }
 
-###6.2 BBOX
+proc bbox_overlap {bbox1 bbox2 {generalized 0}} {
+   foreach i [bbox2points $bbox1] {
+      if {[point_is_in_bbox $i $bbox2 $generalized]} {
+         return true
+      }
+   }
+   foreach i [bbox2points $bbox2] {
+      if {[point_is_in_bbox $i $bbox1 $generalized]} {
+         return true
+      }
+   }
+   return false
+}
+
+###7 LIST
+###7.0 JUDGE
+proc is_list {target} {
+   if {[lindex $target 0] ne ""} {return true}
+   return false
+}
+###7.1 SET THEORY
+proc list_intersection {lA lB} {
+   set sA [lsort -u $lA]
+   set ansList [list]
+   foreach i $sA {
+      if {[lsearch $lB $i]!=-1} {
+         lappend ansList $i
+      }
+   }
+   return $ansList
+}
+
+proc list_diff {lA lB} { ;# A-B
+   set sA [lsort -u $lA]
+   set ansList [list]
+   foreach i $sA {
+      if {[lsearch $lB $i]==-1} {
+         lappend ansList $i
+      }
+   }
+   return $ansList
+}
+
+proc list_union {lA lB} {
+   return [concat [list_diff $lA $lB] $lB]
+}
+
+proc list_product {lA lB} {
+   set r [list]
+   foreach a $lA {
+      foreach b $lB {
+         lappend r [list $a $b]
+      }
+   }
+   return $r
+}
+
+###7.2 MORE LIST OPTIONS
+proc lremove {listVariable value} {
+   upvar 1 $listVariable var
+   set idx [lsearch -exact $var $value]
+   set var [lreplace $var $idx $idx]
+}
+proc lremove_by_index {listVariable idx} {
+   upvar 1 $listVariable var
+   set var [lreplace $var $idx $idx]
+}
+proc push {listVariable item} {
+   upvar 1 $listVariable var
+   set var [concat $var $item]
+}
+proc unshift {listVariable item} {
+   upvar 1 $listVariable var
+   set var [concat $item $var]
+}
+proc pop {listVariable} {
+   upvar 1 $listVariable var
+   set tail [lindex $var end]
+   set var  [lrange $var 0 end-1]
+   return $tail
+}
+proc shift {listVariable} {
+   upvar 1 $listVariable var
+   set head [lindex $var 0]
+   set var [lrange $var 1 end]
+   return $head
+}
+
+###7.3 HISTOGRAM
+# draw_histogram
+proc get_sorted_ll_by_bound {ll bound} {
+   set ind 0
+   foreach i $ll {
+      if {$i>$bound} {break}
+      incr ind
+   }
+   return [lrange $ll 0 [expr $ind-1]]
+}
+proc draw_histogram {ll {from 0} {to "end"} {COLUMN_CNT 10}} {
+   set ll_sort [lsort -dic $ll]
+   set len [llength $ll]
+   set max [stat_max $ll]
+   set min [stat_min $ll]
+
+   set STEP [expr 1.0*($max-$min)/$COLUMN_CNT]
+   puts "max=$max,min=$min"
+   puts "STEP=$STEP"
+   set bound $min
+   set old_bound $bound
+   set cntr 0
+   set old_match_len 0
+   while {$cntr<[expr 2.5*$COLUMN_CNT]} {
+      incr cntr
+      set match_len [llength [get_sorted_ll_by_bound $ll_sort $bound]]
+      puts "\($old_bound,$bound\]: [expr $match_len - $old_match_len]"
+      set old_match_len $match_len
+      set old_bound $bound
+      set bound [expr $bound +$STEP]
+      if {$bound > [expr $max+$STEP]} {
+         break
+      }
+   }
+}
+
+# set ll {-3 1 2 3 4 5 6 7 8 9 10 11 12 1 1 1 1 7 7 7 1.1 1.1 1.1}
+# draw_histogram $ll
+#
+###8 COLLECTION
+###8.0 JUDGE
+proc is_collection {target} {
+   if {[index_collection $target 0] ne ""} {
+      return true
+   } 
+   return false
+}
+###8.1 SET THEORY FOR COLLECTION
+#proc clct_union {} {}
+# ...
+
+###8a GUI
+proc mark_text {} {
+
+}
+
+### FLY LINE:
+#
+
+###9 ABSTRACT ALGRBRA
+proc transformation {vt v2} {
+# transformation {0 1 2} {a b c} => {a b c}
+# transformation {1 2 0} {a b c} => {c a b}
+   if {[llength $vt]!=[llength $v2]} {
+      puts "Error: proc transformation: llength vt, v2 not equal"
+   }
+   set new_ind 0
+   set new_l [list]
+   while {$new_ind<[llength $v2]} {
+      set old_ind [lsearch $vt $new_ind]
+      lappend new_l [lindex $v2 $old_ind]
+      incr new_ind
+   }
+   return $new_l
+}
+proc rand_transformation {{length 1}} { 
+;# in tcllib there is a shuffle proc
+   set r [list]
+   set cnt 0
+   while {[llength $r]<$length} {
+      set seed [expr int(rand()*$length)] ;# 0~$length-1
+      if {[lsearch $r $seed]==-1} {
+         lappend r $seed
+      }
+      incr cnt
+      if {$cnt>[expr 1e3*$length]} {puts "Error: iterating $cnt times";break}
+   }
+   return $r
+}
+
+###10 FORMAT PRINTING
+proc putsd {info} {
+   if {[getenv DEBUG]} {
+      puts $info
+      if {![file exists [getenv CONTINUE_FLAG_FILE]]} {
+         set sh_script_stop_severity E
+         puts "[getenv CONTINUE_FLAG_FILE] does not exist! stop here!"
+         script stop here
+      }
+   }
+}
+
+###11 DEBUG TOOL 
+proc tracer {varName args} {
+   upvar $varvame var
+   puts "DEBUG Info: tracer: $varName was updated to be \"$var\""
+}
+if {0} {;# use tracer
+   trace variable var_example wu tracer
+   trace vinfo var_example
+   trace vdelete var_example wu command
+}
+
+proc debug_proc_tracer args {
+   puts "debug_proc_tracer $args"
+}
+proc debug_add_trace_to_proc {name {yesno 1}} {
+   set mode [expr {$yesno? "add":"remove"}]
+   trace $mode execution $name {enter leave} debug_proc_tracer
+}
+
+proc time_flag {flag} {
+   putsd "TIME_FLAG: $flag - [debug timestamp]"
+}
+
+###12 OO
+if {0} {
+   itcl::find class tclass
+   itcl::delete class tclass
+   source xxx/xxx/itcl.tcl
+}
+
+proc make_obj {class_name obj_name} {
+   set name $obj_name
+   if {[itcl::find object $name -class $xlass_name] ne ""} {
+      itcl::delete object $name
+   }
+   $class_name $name
+}
+
+###13 tcllib 
+# move to head of this file
+
+###14 PATH
+proc lappend_PATH {path_name dir} {
+   upvar 1 $path_name var
+   if {[lsearch $var $dir]==-1} {
+      lappend var $dir
+   }
+}
+###15 FILE RELATED
+proc grep {re args} {
+   set return_flag 0
+   set files [eval glob -types f $args]
+   foreach file $files {
+      set fp [open $file]
+      while {[gets $fp line]>=0} {
+         if [regexp -- $re $line] {
+            if {[llength $files]>1} {puts -nonewline $file:}
+            puts $line
+            set return_flag 1
+         }
+      }
+      close $fp
+   }
+   return $return_flag
+}
+
+###16 TIMING PATh
+proc get_timing_path_segment {from to {include_hierarchical_pins 1}} {
+   if {$include_hierarchical_pins} {
+      set path_clct [get_timing_paths -thr $from -to $to -include]
+   } else {
+      set path_clct [get_timing_paths -thr $from -to $to ]
+   }
+   if {[sizeof_coll $path_clct]==0} {
+      puts "Warning: proc get_timing_path_segment: no path from $from to $to"
+      return
+   }
+   set path [index_collection $path_clct 0]
+
+   set flag_in_segment 0
+   set flag_in_segment_buf 0
+   set ansList [list]
+
+   foreach_in_c p [get_attr $path points] {
+      set obj [get_attr $p object]
+      set point_name [get_attr $obj full_name]
+
+      set suffix ""
+      if {$point_name eq $from} {
+         set flag_in_segment 1
+         set flag_in_segment_buf 1
+         set suffix "<-"
+      }
+      if {$point_name eq $to} {
+         set flag_in_segment_buf 0
+         set suffix "<-"
+      }
+
+      if {$flag_in_segment} {
+         lappend ansList $point_name
+      }
+      set flag_in_segment $flag_in_segment_buf
+   }
+   return $ansList
+}
+
+
+###17 TECH RELATED
+proc get_bitcnt {ref_name} {
+   puts "calling proc get_bitcnt $ref_name"
+}
+
+proc sum_of_bitcnt {cell_list} {
+   set sum 0
+   foreach c $cell_list {
+      set ref [get_attr $c ref_name]
+      incr sum [get_bitcnt $ref]
+   }
+   return $sum
+}
+
